5aa819ab8d368b46644b24bb299208dcbb6a3a83
bibin thomas <82058754+bibinthomas123@users.noreply.github.com>
2022-07-12 14:22:12 +0530

    removed api

--- project/test.py
+++ project/test.py
@@ -1,25 +1,3 @@
-# import http.client, urllib.parse
-# import json
-
-# conn = http.client.HTTPConnection('geocode.xyz')
-
-# params = urllib.parse.urlencode({
-#     'auth': '156997931047459624760x95680',
-#     'locate': 'chintal ',
-#     'region': 'IN',
-#     'json': 1,
-#     })
-
-# conn.request('GET', '/?{}'.format(params))
-
-# res = conn.getresponse()
-# data = res.read()
-# json_data = json.loads(data)
-# print(json_data["longt"])
-# print(json_data["latt"])
-
-# print(data)
-
 import http.client
 
 
